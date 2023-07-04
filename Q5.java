cf7985124f4cea1e2c4ebd3200225de19aef6a8b
prayashkumarsingh <88892535+prayashkumarsingh@users.noreply.github.com>
2023-07-04 13:12:22 +0530

    Add files via upload

---
+++ Q5.java
@@ -0,0 +1,12 @@
+public class Q5 {
+
+    public static void main(String[] args) {
+        int my_integer = 10;
+        double my_double = 10.5;
+        String my_string = "Hello, world!";
+
+        System.out.println("my_integer = " + my_integer);
+        System.out.println("my_double = " + my_double);
+        System.out.println("my_string = " + my_string);
+    }
+}
